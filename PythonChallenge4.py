db638a7ce012b66d00b1d63c3d8bafc1bb67a02a
Ryan <bennisryanj@gmail.com>
2026-01-07 20:45:50 -0500

    Pig Latin Word Translator

--- PythonChallenge4.py
+++ PythonChallenge4.py
@@ -1,1 +1,17 @@
 
+valildword = input()
+
+
+def pigLatin(word):
+    firstLetter = word[0]
+    vowels = ["a", "e", "i", "u"]
+    newWord = ""
+    if firstLetter in vowels:
+        newWord = word + "way"
+        print(newWord)
+    else:
+        newWord = word[1:] + word[0] + "ay"
+        print(newWord)
+
+
+pigLatin(valildword)
